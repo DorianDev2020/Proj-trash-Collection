09247e7107e8460722b52b5c87d52b9a34d0ded2
DorianHaley <59707012+DorianHaley@users.noreply.github.com>
2020-02-19 10:34:03 -0600

    Sucessfully added migration, and updated database

---
+++ Data/Migrations/20200219163312_Added Foreign Keys.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjTrashCollection.Data.Migrations
+{
+    public partial class AddedForeignKeys : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "14c6e95e-e063-494a-8c94-9e3feec18893");
+
+            migrationBuilder.AddColumn<string>(
+                name: "IdentityUserId",
+                table: "Employees",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "IdentityUserId",
+                table: "Customers",
+                nullable: true);
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "c9cb1197-383e-42ea-be92-7c9ebd2d6136", "bf6471a0-c853-4af8-a6c1-bea1aad45360", "Admin", "ADMIN" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_IdentityUserId",
+                table: "Employees",
+                column: "IdentityUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_IdentityUserId",
+                table: "Customers",
+                column: "IdentityUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Customers_AspNetUsers_IdentityUserId",
+                table: "Customers",
+                column: "IdentityUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_AspNetUsers_IdentityUserId",
+                table: "Employees",
+                column: "IdentityUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Customers_AspNetUsers_IdentityUserId",
+                table: "Customers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_AspNetUsers_IdentityUserId",
+                table: "Employees");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Employees_IdentityUserId",
+                table: "Employees");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Customers_IdentityUserId",
+                table: "Customers");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "c9cb1197-383e-42ea-be92-7c9ebd2d6136");
+
+            migrationBuilder.DropColumn(
+                name: "IdentityUserId",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "IdentityUserId",
+                table: "Customers");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "14c6e95e-e063-494a-8c94-9e3feec18893", "916d4095-5e55-44fd-ab76-072285ee6f30", "Admin", "ADMIN" });
+        }
+    }
+}
