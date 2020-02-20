ce2c7a0adebe7e07b8e5e1587f8083000aa70841
DorianHaley <59707012+DorianHaley@users.noreply.github.com>
2020-02-20 14:01:21 -0600

    Completed Services Controller

---
+++ Controllers/ServicesController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ProjTrashCollection.Data;
+using ProjTrashCollection.Models;
+
+namespace ProjTrashCollection.Controllers
+{
+    public class PickupsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PickupsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Pickups
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Service.ToListAsync());
+        }
+
+        // GET: Pickups/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var service = await _context.Service
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (service == null)
+            {
+                return NotFound();
+            }
+
+            return View(service);
+        }
+
+        // GET: Pickups/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Pickups/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,PickupDay,IsSuspended,Start,End,OneTimePickup")] Service service)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Service.Add(service);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", "Customers");
+            }
+            return View(service);
+        }
+
+        // GET: Pickups/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var service = await _context.Service.FindAsync(id);
+            if (service == null)
+            {
+                return NotFound();
+            }
+            return View(service);
+        }
+
+        // POST: Pickups/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,PickupDay,IsSuspended,SuspendedStart,SuspendedEnd,OneTimePickup")] Service service)
+        {
+            if (id != service.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(service);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PickupExists(service.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(service);
+        }
+
+        // GET: Pickups/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var service = await _context.Service
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (service == null)
+            {
+                return NotFound();
+            }
+
+            return View(service);
+        }
+
+        // POST: Pickups/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var service = await _context.Service.FindAsync(id);
+            _context.Service.Remove(service);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PickupExists(int id)
+        {
+            return _context.Service.Any(e => e.Id == id);
+        }
+    }
+}
