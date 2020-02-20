abaae11859f7847fad0c80f779f465f95ee8d165
DorianHaley <59707012+DorianHaley@users.noreply.github.com>
2020-02-20 08:25:56 -0600

    Created A Service Model

---
+++ Models/Service.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjTrashCollection.Models
+{
+    public class Service
+    {
+        [Key]
+        public double Balance { get; set; }
+        [Display(Name = "Start Date")]
+        public DateTime StartDate { get; set; }
+        [Display(Name = "End Date")]
+        public DateTime EndDate { get; set; }
+        [Display(Name = "Pick Up Day")]
+        public string PickUpDay { get; set; }
+        [Display(Name = "One Time Pick Up")]
+        public DateTime OneTimePickUp { get; set; }
+        public bool IsSuspended { get; set; }
+    }
+}
